27bd865bc9dd32d0492b6a4ff7001f894e99c712
Sujan-1990 <sujrap@gmail.com>
2024-06-19 18:47:52 +0545

    Added Header file and navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 //import Header from "./components/Header";
-import Home from "./components/pages/Home";
-import Contact from "./components/pages/Contact";
-import Products from "./components/pages/Products";
-import About from "./components/pages/About";
-import Cart from "./components/pages/Cart";
-import SingleProduct from "./components/pages/SingleProduct";
-import ErrorPage from "./components/pages/ErrorPage";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import SingleProduct from "./pages/SingleProduct";
+import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
 // -----------------------------------------
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<>
 			<Router>
+				<Header />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,34 @@
-import { Link } from "react-router-dom";
-import Button from "./Button";
+import { Link, NavLink } from "react-router-dom";
+// import Button from "./Button";
 import "./header.css";
+import { FiShoppingCart } from "react-icons/fi";
+
 const Header = () => {
 	return (
 		<nav>
-			<div className="navbar">
-				<div className="wrapper">
-					<div className="logo">
-						<Link className="link" to="/">
-							CoMpAnY LoGo
-						</Link>
-					</div>
-
-					<div className="navbar-right">
-						<div className="items">
-							<Link className="link" to="/">
-								Home
-							</Link>
-							<Link className="link" to="/products">
-								Products
-							</Link>
-							<Link className="link" to="/about">
-								About
-							</Link>
-							<Link className="link" to="/contact">
-								Contact Us
-							</Link>
-							<Button name="Login" btnClass="login__btn" />
-						</div>
-					</div>
-				</div>
+			<div className="nav__left">
+				<NavLink className="links" to="/">
+					<h4>LoGo</h4>
+				</NavLink>
+			</div>
+			<div className="nav__right">
+				<NavLink className="links" to="/">
+					Home
+				</NavLink>
+				<NavLink className="links" to="/about">
+					About Us
+				</NavLink>
+				<NavLink className="links" to="/products">
+					Products
+				</NavLink>
+				<NavLink className="links" to="/contact">
+					Contact
+				</NavLink>
+				{/* <Button name="Login" btnClass="login__btn" /> */}
+				<NavLink className="links cart-trolley--link" to="/cart">
+					<FiShoppingCart className="cart-trolley" />
+					<span className="cart_total_item">10</span>
+				</NavLink>
 			</div>
 		</nav>
 	);
